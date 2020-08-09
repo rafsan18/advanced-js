4d69502fc5aa85d97729498ecd5876777508c5ee
rafsan <rafsanamin18@gmail.com>
2020-08-09 14:43:04 +0600

    24.3 null vs undefined

---
+++ undefined-null.js
@@ -0,0 +1,42 @@
+// getting undefined
+//1
+// let pakhi;
+// console.log(pakhi);
+
+//2
+// if function is not return then it will give undefined value
+// function add(num1, num2) {
+//   console.log(num1 + num2);
+// }
+// const result = add(12, 15);
+// console.log(result);
+
+// 3
+// function add(num1, num2) {
+//   console.log(num1 + num2);
+//   return;
+// }
+// const result = add(12, 15);
+// console.log(result);
+
+// 4
+// function add(num1, num2) {
+//   console.log(num1, num2);
+// }
+// const result = add(12);
+// console.log(result);
+
+// 5
+// premik is an object which have property name and phone
+// if a property is called from object which is not present in object it will give undefined
+// const premik = { name: "smart dude", phone: 458254 };
+// console.log(premik.gf);
+
+// 6
+// let fun = undefined;
+// console.log(fun);
+
+// getting null
+
+let fun = null;
+console.log(fun);
